250a9930499f464b1ccd7156a6e75c77dc36983d
GuPimenta <150020200+GuPimenta@users.noreply.github.com>
2024-04-02 15:57:26 -0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,29 +10,32 @@ public class Program
 	{
 		Console.WriteLine("CONVERSOR DE UM BINÁRIO DE 4 BITS\n");
 		List<int> Bin = new List<int>();
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 4; i++) //Informando os 4 binários
 		{
 			Console.Write($"Informe o {i + 1}º bit: ");
 			Bin.Add(int.Parse(Console.ReadLine()));
 		}
 
-		String Temp = String.Join("", Bin);
-		int Decimal = Convert.ToInt32(Temp, 2);
-		string Octal = Convert.ToString(Decimal, 8);
-		string Hexa = Convert.ToString(Decimal, 16).ToUpper();
+		String Temp = String.Join("", Bin); //Concatenando os Bits
+		int Decimal = Convert.ToInt32(Temp, 2); //Converção pra Decimal
+		string Octal = Convert.ToString(Decimal, 8); //Converção pra Octal
+		string Hexa = Convert.ToString(Decimal, 16).ToUpper(); //Converção pra Hexa
 		Console.WriteLine("\nBinário informado: " + Temp);
 		Console.WriteLine("Convertido para Decimal: " + Decimal);
 		Console.WriteLine("Convertido para Octal: " + Octal);
 		Console.WriteLine("Convertido para Hexa: " + Hexa);
 		Console.WriteLine("\n A   B   C   D   AND   OR");
-		Console.WriteLine($" {Bin[0]} | {Bin[1]} | {Bin[2]} | {Bin[3]}" + "    " + (Bin[0] & Bin[1] & Bin[2] & Bin[3]) + "    " + (Bin[0] | Bin[1] | Bin[2] | Bin[3]));
+		Console.WriteLine($" {Bin[0]} | {Bin[1]} | {Bin[2]} | {Bin[3]}" + 
+		"    " + (Bin[0] & Bin[1] & Bin[2] & Bin[3]) + 
+		"    " + (Bin[0] | Bin[1] | Bin[2] | Bin[3]));
+		
 		Console.WriteLine("\nTABELA VERDADE");
 		Console.WriteLine(" A   B   C   D     S = (A.B.C.D)   S = (C+D).(A.C)   S = (A+B+C+D)   S = (A+B)+(C.D)");
-		int[] A = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+		int[] A = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]; //Criação de tabela verdade
 		int[] B = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1];
 		int[] C = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1];
 		int[] D = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
-		for (int i = 0; i < A.Length; i++)
+		for (int i = 0; i < A.Length; i++) //Execução das operações
 		{
 			Console.WriteLine($" {A[i]} | {B[i]} | {C[i]} | {D[i]}" + 
 			"            " + (A[i] & B[i] & C[i] & D[i]) + 
